156990cf9af26d24a5487c1b2c352b23dfdd921d
Matheus Germano <matheusgermano.cp@outlook.com>
2023-02-13 23:17:36 -0300

    update

---
+++ scripts/dot.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function clop(){
+    path+="$1/"
+    echo "-------------------------------"
+    echo "$path"
+    
+    for FILE in $path{.[!.]*,*}; do
+        f=$(basename $FILE)
+        checkpath="$HOME/${FILE#$basepath/}"
+        echo "checkpath: $checkpath"
+
+        [[ -f "$checkpath" ]] && cp $checkpath $path 
+        if [[ -d "$checkpath" ]]; then 
+            clop $f
+        fi
+    done
+    path=${path%$1/}
+    echo "$path"
+}
+
+basepath=$HOME/dotfiles
+path=$basepath
+
+if [[ $#  -eq 0 ]]; then
+    echo "-------------------------"
+    echo "Wrong Use"
+    echo "You must give a parameter"
+    echo "-------------------------"
+
+elif [[ $1 == "source" ]]; then
+    clop
+else
+    if [[ -d $1 ]]; then
+        cp -r $1 /home/matt/dotfiles/$2/$1
+    else
+         cp $1 /home/matt/dotfiles/$2/$1
+    fi
+fi
+
